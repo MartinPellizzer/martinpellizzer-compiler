033e2346b622a0a65fcece1dc96fd8fc948bcac5
Martin Pellizzer <martinpellizzer@gmail.com>
2023-05-08 16:14:56 +0200

    '

--- push.bat
+++ push.bat
@@ -3,12 +3,3 @@ python main.py
 git add .
 git commit -m "'"
 git push
-
-robocopy .\public ..\mp\ /E
-cd ..
-cd mp
-git add .
-git commit -m "'"
-git push
-cd ..
-cd mp_wb_compiler
---
+++ upload.bat
@@ -0,0 +1,8 @@
+robocopy .\public ..\mp\ /E
+cd ..
+cd mp
+git add .
+git commit -m "'"
+git push
+cd ..
+cd martinpellizzer_compiler
